cac33d942a37e0b2065d72f91330dcac70d29033
whisper-xiang <xiangergou000@gmail.com>
2024-08-08 23:22:34 +0800

    feat: 骨架搭建

---
+++ src/components/assets/Popup/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import { openOptionsPage } from "../../lib/brower";
+// import Example from "@/components/Example";
+// import AccountList from "../AccountList";
+
+export default function Popup() {
+  return <div style={{ width: "500px" }}>test</div>;
+}
